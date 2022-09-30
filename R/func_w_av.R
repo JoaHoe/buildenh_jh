ef0efdf7ff187017a208d6200b8be34f463beb46
ImgEnh <jkhoehle@gmail.com>
2022-09-30 16:53:11 +0200

    Create func_w_av.R

---
+++ R/func_w_av.R
@@ -0,0 +1,21 @@
+## script:'func_w_av.R'
+#purpose: weighted average of theta-angle (theta_av)
+#used in: intersect_corner_points.R
+
+#function
+w_av <- function(ang,len) { 
+  x <- length(ang)
+  y <- 1 : x
+  pw <- len/max(len) #weights
+  sp <- sum(pw)
+  theta <- rep(0,x)
+  
+  for (i in y) {
+    theta[i] <- (ang[i]*pw[i])/sp
+  }
+  
+  theta_av <- sum(theta)
+  return(theta_av)
+} #end of function 'w_av(ang,len)'
+
+#end of script 'func_w_av.R
