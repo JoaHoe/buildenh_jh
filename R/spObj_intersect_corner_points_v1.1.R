f4c063eb25c1fe7a63fda06377db77cba17e5980
ImgEnh <jkhoehle@gmail.com>
2022-09-30 16:55:04 +0200

    Create spObj_intersect_corner_points_v1.1.R

---
+++ R/spObj_intersect_corner_points_v1.1.R
@@ -0,0 +1,15 @@
+##name of script 'spObj_intersect_corner_coordinates.R'
+cat("version_number= ",v_nr,"\n")
+## purpose: average angle for objects with non-orthogonal lines 
+
+## ISPRS_#7
+if (Img_name == "ISPRS7") { 
+  #no corrections
+} #end ISPRS7
+#
+
+if (Img_name == "ISPRS1") { 
+  #no corrections
+} #end ISPRS1
+#######################################################
+## end of spObj_intersect_corner_points.R
