69c92986a9b98b8e16ece4da2195d15abfbbd51f
ImgEnh <jkhoehle@gmail.com>
2022-09-30 16:50:58 +0200

    Create func_loadLib_jh.R

---
+++ R/func_loadLib_jh.R
@@ -0,0 +1,30 @@
+## name of script: func_loadLib_jh.R
+## purpose: collection of all functions for package 'buildenh'
+
+loadLib_jh <- function() { 
+  source("func_adjust_coord.R")
+  source("func_corner_name.R")
+  source("func_design_mat.R")
+  source("func_design_mat2.R")
+  source("func_det_of_angle.R")
+  source("func_detect_meas1.R")
+  source("func_dist_PC.R")
+  source("func_dist_v.R")
+  source("func_dist_v2.R")
+  source("func_fe.R")
+  source("func_line_reduce.R")
+  source("func_locator2.R")
+  source("func_mean_line.R")
+  source("func_new_centre.R")
+  source("func_new_centre_auto.R")
+  source("func_PC_segment_4.R")
+  source("func_rectang_lines.R")
+  source("func_reduce_pointset.R")
+  source("func_test_gaps_onedirection.R")
+  source("func_detect_thet_len.R")
+  source("func_trans_ortho.R")
+  source("func_trans_H_res.R")
+  source("func_w_av.R")
+} #end of function 'loadLib_jh()'
+
+#end of script 'func_loadLib_jh.R'
