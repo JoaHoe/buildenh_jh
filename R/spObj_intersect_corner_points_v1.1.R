b55a274ec685ad9531272628f27f8612cf235610
JoaHoe/buildenh_jh <jh@gaf-net.dk>
2022-12-10 21:00:49 +0100

    Update spObj_intersect_corner_points_v1.1.R

--- R/spObj_intersect_corner_points_v1.1.R
+++ R/spObj_intersect_corner_points_v1.1.R
@@ -10,7 +10,12 @@ if (Img_name == "ISPRS7") {
 #
 
 if (Img_name == "ISPRS1") { 
-  #no corrections
+  
+  #b36
+  if (bnr2==36 && p_pos == "cor_theta_av2") { 
+    theta_av2_mod <- 71.4769 #manual calculation by support_intersect_corner_points #4
+  } #end if
+
 } #end ISPRS1
 #######################################################
 ## end of spObj_intersect_corner_points.R
