c045529ee20227bb5a759de815a741cc80299385
JoaHoe/buildenh_jh <jh@gaf-net.dk>
2023-01-22 21:43:22 +0100

    Update spObj_intersect_corner_points_v1.1.R

--- R/spObj_intersect_corner_points_v1.1.R
+++ R/spObj_intersect_corner_points_v1.1.R
@@ -15,6 +15,25 @@ if (Img_name == "ISPRS1") {
   if (bnr2==36 && p_pos == "cor_theta_av2") { 
     theta_av2_mod <- 71.4769 #manual calculation by support_intersect_corner_points #4
   } #end if
+  
+  #b372
+  
+  if (bnr2==372 && p_pos == "cor_theta_av") {
+    theta_av_mod <- theta_ref_adj
+    theta_av_mod
+  } #end if
+  
+  if (bnr2==372 && p_pos == "cor_theta_av2") {
+    cat("i2 =", i2,"\n") 
+    theta_av2_mod <- B6_seq$theta_adj[i2]
+  } #end if
+  
+  #b45
+  if (bnr2==45 && p_pos == "cor_theta_av") {
+    theta_av_mod <- theta_av_mod
+  } #end if
+  
+  
 
 } #end ISPRS1
 #######################################################
