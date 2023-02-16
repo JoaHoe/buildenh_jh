1e3a700d7a6de4f29ea17498182c2ea901812f19
JoaHoe/buildenh_jh <jh@gaf-net.dk>
2023-02-16 10:36:12 +0100

    Update support_adjustment_of_line_v1.1.R

--- R/support_adjustment_of_line_v1.1.R
+++ R/support_adjustment_of_line_v1.1.R
@@ -1,22 +1,22 @@
 ## name of script: support_adjustment_of_line.R
-## purpose: supporting software for special objects
-## GNU General Public License (GPL)
-
 cat("version_number= ",v_nr,"\n")
-
+## purpose: supporting software for special objects
+## author: Joachim Höhle
+## GNU General Public License (GPL)
 ## contents:
 # 1.adjustment of all lines stored in matrix 'B6_seq' 
-# 2.adjustment of the four longest lines with 'theta_ref','alph_ref' and weighted average (theta_av)
-# 3.adjustment of longest line (lnr=1)
+# 2.adjustment of four calculated lines with theta_ref and alpha_ref + weighted mean
+# 3.adjustment of selected reference line
 ################################################################################
 
 ## 1.adjustment of all lines stored in matrix 'B6_seq'
 # derivation of line parameters (with residuals orthogonal to line)
 # calculation in math system
 
-B6 <- B6_seq
+B6_seq #line #103 in script 'adjustment of line'
+B6 <- B6_seq 
 B6
-x <- length(B6$lnr)
+x <- length(B6$PC_nr)
 row.names(B6) <- seq(x)
 y2 <- 1:x
 
@@ -52,14 +52,14 @@ for (i in y2) {
 
 ################################################################################
 
-## 2.adjustment of four longest lines with theta_ref and alpha_ref + weighted mean
-#read in the four long lines with theta_ref and alpha_ref
-setwd(home_dir)
+## 2.adjustment of four calculated lines with theta_ref and alpha_ref + weighted mean
+#to be used for object types "extr_wd" and "4_long" only
+
 points_all <- seq(4)
 phi_all <- seq(4)
 z=1
+B5_long_lines
 y <- B5_long_lines$lnr[1:4]
-#
 
 for (i in y) {
   lnr <- i
@@ -93,6 +93,7 @@ for (i in y) {
   points_all[z] <- B5_long_lines$n_pixel[z]
   z <- z+1
 }
+
 phi_all
 points_all #length of lines
 
@@ -104,20 +105,17 @@ phi_w <- weights * phi_all / sum(weights)
 phi_weighted_mean <- sum(phi_w)
 cat("weighted mean of 4 longest lines=",phi_weighted_mean,"degrees","\n")
 theta_av <- 90 - phi_weighted_mean
-fname10 <- paste("./data/",Img_name,"/theta_av_b",bnr2,".txt",sep="")
-write.table(theta_av,fname10)
-#
+cat("theta_average= ", theta_av,"degrees","\n")
 
 #end of script 2
 ################################################################################
 
-## 3.adjustment of longest line (lnr=1)
+## 3.adjustment of selected reference line
 
 #read ref-line
-setwd(home_dir)
-bnr2 <- 18 #to be changed, for demos: 18(ISPRS7), 11(ISPRS1) 
+bnr2 <- 7 #to be changed, for demos: 18(ISPRS7), 11(ISPRS1) 
 z <- 1
-y <- B6$lnr[6]
+y <- B4$lnr[8] #change to selected ref-line number (index)
 
 for (i in y) {
   lnr <- i
@@ -136,28 +134,26 @@ for (i in y) {
     j <- j+1
     x <- PC$x[j]
     y <- PC$y[j]
-    points(x,-y, pch=16, cex=0.4, col="red", asp=1)
+    points(x,-y, pch=16, cex=0.4, col="black", asp=1)
   }
   
   x_dat <- PC$x
   y_dat <- (-PC$y) #change to math system
   xs <- sum(x_dat)/k4
   ys <- sum(y_dat)/k4
-  x_dat_v <- x_dat-xs
-  y_dat_v <- y_dat-ys
+  x_dat_v <- x_dat - xs
+  y_dat_v <- y_dat - ys
   N <- (t(x_dat_v) %*% x_dat_v-t(y_dat_v) %*% y_dat_v)
   phi_2 <- (2*t(x_dat_v) %*% y_dat_v)/N
   phi_2_deg <- omega*atan(phi_2) #two solutions (solution 1 or 2) 
-# are possible for phi_2 (phi_2 or phi_2 + 180)
+  #two solutions are possible for phi_2 (phi_2 or phi_2 + 180)
   phi_deg <- 0.5 * (phi_2_deg) #calculation of angle phi (slope angle)
-  cat("line=",lnr, "  phi_deg=",phi_deg, "xs=", xs, "ys=", ys ,"\n")
+  cat("ref-line= ",lnr, "  phi_deg=",phi_deg, "xs=", xs, "ys=", ys ,"\n")
 }
 
-theta_1 <- 90 - phi_deg #solution
+theta_ref_adj <- 90 - phi_deg #solution
+cat("theta_adj= ",theta_ref_adj,"degrees","\n")
 
-#output of result
-fname10 <- paste("./data/",Img_name,"/theta_b",bnr2,"_1.txt",sep="")
-write.table(theta_1,fname10)
 #end of script 3
 ################################################################################
 
